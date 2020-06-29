bac038c6819b6995066bcb7b802797eea68b409d
Carol Li <caroldotli@gmail.com>
2020-06-28 21:44:09 -0400

    Finish todo service

--- todo-app/src/app/classes/todo/todo-data.service.ts
+++ todo-app/src/app/classes/todo/todo-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
+import { __assign } from 'tslib';
 
 @Injectable({
   providedIn: 'root'
@@ -29,6 +30,34 @@ export class TodoDataService {
   deleteTodoById(id: number): TodoDataService {
     this.todos = this.todos.filter(todo => todo.id !== id);
     return this;
+  }
+
+  // PUT /todos/:id
+  updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // GET /todos/:id
+  getTodoById(id: number): Todo {
+    return this.todos.filter(todo => todo.id === id).pop();
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo) {
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
   }
 
 }
